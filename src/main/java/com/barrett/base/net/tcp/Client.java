7f6b4d131d7a6acb1d5b2fca763794ea81e0dbd7
yicheng.tan <yicheng.tan@seraphim-energy.com>
2020-05-25 23:01:52 +0800

    feat: tcp聊天demo

--- src/main/java/com/barrett/base/net/tcp/Client.java
+++ src/main/java/com/barrett/base/net/tcp/Client.java
@@ -8,26 +8,27 @@ import java.net.Socket;
 
 /**
  * 客户端
+ *
  * @Author created by barrett in 2020/5/24 14:29
  */
 public class Client {
     public static void main(String[] args) {
-        clientFile();
+        new Client().clientMulit();
     }
 
 
     //上传文件
-    public static void clientFile(){
+    public static void clientFile() {
         try {
             //建立连接
-            Socket client = new Socket("127.0.0.1",9999);
+            Socket client = new Socket("127.0.0.1", 9999);
             //输入输出流操作
             InputStream is = new BufferedInputStream(new FileInputStream(Constants.file_path_man));
             BufferedOutputStream bos = new BufferedOutputStream(client.getOutputStream());
             byte[] bytes = new byte[1024];
-            int len=-1;
-            while ((len=is.read(bytes))!=-1){
-                bos.write(bytes,0,len);
+            int len = -1;
+            while ((len = is.read(bytes)) != -1) {
+                bos.write(bytes, 0, len);
             }
             bos.flush();
 
@@ -40,21 +41,96 @@ public class Client {
         }
     }
 
-    public static void client(){
+    public static void client() {
         try {
             //建立连接
-            Socket client = new Socket("127.0.0.1",9999);
+            Socket client = new Socket("127.0.0.1", 9999);
             //输入输出流操作
-             DataOutputStream dos = new DataOutputStream(client.getOutputStream());
-             String str = "hello";
-             dos.writeUTF(str);
-             dos.flush();
-             //释放资源
+            DataOutputStream dos = new DataOutputStream(client.getOutputStream());
+            String str = "hello";
+            dos.writeUTF(str);
+            dos.flush();
+            //释放资源
             dos.close();
             client.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void clientMulit() {
+        try {
+            //通过控制台输入
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            //建立连接
+            Socket client = new Socket("127.0.0.1", 9999);
+
+            //发送
+            String s1 = reader.readLine();
+            new Send(client).send(s1);
+
+            //接收服务端消息
+            new Receive(client).receive();
+
+            //释放资源
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class Send {
+        private DataOutputStream dos;
+        private Socket client;
+
+        public Send(Socket client) {
+            this.client = client;
+            try {
+                this.dos = new DataOutputStream(client.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void send(String msg) {
+
+            //输入输出流操作
+            try {
+                dos = new DataOutputStream(client.getOutputStream());
+//                String str = "hello";
+                dos.writeUTF(msg);
+                dos.flush();
+//                dos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class Receive {
+        private Socket client;
+        private DataInputStream is;
+
+        public Receive(Socket client) {
+            this.client = client;
+            try {
+                this.is = new DataInputStream(client.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void receive() {
+            try {
+                String s = is.readUTF();
+                System.out.println(s);
+
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
--- src/main/java/com/barrett/base/net/tcp/Server.java
+++ src/main/java/com/barrett/base/net/tcp/Server.java
@@ -6,6 +6,7 @@ import com.barrett.common.constants.Constants;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.Channel;
 
 /**
  * 指定端口 使用serverSocket 创建服务器
@@ -15,10 +16,10 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) {
-        serverFile();
+        new Server().serverMulit();
     }
 
-    public static void serverFile(){
+    public void serverFile(){
         try {
             //1、指定端口 使用serverSocket 创建服务器
             ServerSocket server = new ServerSocket(9999);
@@ -44,7 +45,8 @@ public class Server {
             e.printStackTrace();
         }
     }
-    public static void server(){
+
+    public void server(){
         try {
             //1、指定端口 使用serverSocket 创建服务器
             ServerSocket server = new ServerSocket(9999);
@@ -62,6 +64,88 @@ public class Server {
             socket.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * 处理多个客户端
+     * @Author created by barrett in 2020/5/25 21:50
+     */
+    public void serverMulit(){
+        try {
+            //1、指定端口 使用serverSocket 创建服务器
+            ServerSocket server = new ServerSocket(9999);
+
+            //2、阻塞式等待连接 accept
+            while(true){
+                Socket socket = server.accept();
+                System.out.println("一个客户端连接。。。");
+                Channel channel = new Channel(socket);
+                new Thread(channel).start();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class Channel implements Runnable{
+
+        private  DataInputStream dis;
+        private  DataOutputStream dos;
+        private Socket client;
+
+        //通过构造器初始化数据
+        public Channel( Socket client) {
+            try {
+                this.client = client;
+                this.dis = new DataInputStream(client.getInputStream());
+                this.dos = new DataOutputStream(client.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+                try {
+                    client.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        }
+
+        //接收数据
+        public String resive(){
+             String s="";
+            try {
+                s = dis.readUTF();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return s;
+        }
+
+        @Override
+        public void run() {
+
+            String str = "";
+
+            String data = resive();
+            System.out.println(data);
+
+            //写出到客户端
+            try {
+                dos.writeUTF("ok");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            //4、释放资源
+            try {
+                dos.close();
+                dis.close();
+                client.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
