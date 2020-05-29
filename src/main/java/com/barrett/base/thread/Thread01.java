99d37ed21b267f49b90d3008948915fc32630621
yicheng.tan <yicheng.tan@seraphim-energy.com>
2020-05-29 22:38:14 +0800

    feat: 继承方式实现多线程

--- src/main/java/com/barrett/base/thread/Thread01.java
+++ src/main/java/com/barrett/base/thread/Thread01.java
@@ -1,4 +1,32 @@
 package com.barrett.base.thread;
 
+/**
+ * 多线程
+ * @Author created by barrett in 2020/5/29 20:33
+ */
 public class Thread01 {
+
+    public static void main(String[] args) {
+        new MyThread().start();
+    }
+
+    /**
+     * 继承方式实现
+     * @Author created by barrett in 2020/5/29 22:37
+     */
+    static class MyThread extends Thread{
+
+        @Override
+        public void run() {
+            System.out.println("thread 子线程");
+        }
+    }
+
+    static class MyThead2 implements Runnable{
+
+        @Override
+        public void run() {
+
+        }
+    }
 }
