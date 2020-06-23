5712383a4a3bdff1bca88b53695d0f3b5ecf728b
barrett <git config --global user.email>
2020-06-23 08:32:08 +0800

    docs: nothing

--- src/main/java/com/barrett/common/constants/Constants.java
+++ src/main/java/com/barrett/common/constants/Constants.java
@@ -3,5 +3,5 @@ package com.barrett.common.constants;
 public interface Constants {
     String file_path_man = "src/main/resources/img/man.gif";
 
-    String test = "今天学习 flutter";
+    String test = "今天学习 flutter  ";
 }
