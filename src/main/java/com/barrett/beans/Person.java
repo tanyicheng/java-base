73c9b84fab03f876549174e05fad02710151073f
barrett <git config --global user.email>
2020-06-28 08:22:12 +0800

    feat: list 测试

---
+++ src/main/java/com/barrett/beans/Person.java
@@ -0,0 +1,68 @@
+package com.barrett.beans;
+
+public class Person {
+    private Integer id;
+    private String name;
+    private String age;
+    private String address;
+    private String iphone;
+
+    public Person(Integer id, String name, String age, String address, String iphone) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.iphone = iphone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getIphone() {
+        return iphone;
+    }
+
+    public void setIphone(String iphone) {
+        this.iphone = iphone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", address='" + address + '\'' +
+                ", iphone='" + iphone + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/barrett/test/TestList.java
@@ -0,0 +1,60 @@
+package com.barrett.test;
+
+import com.barrett.beans.Person;
+
+import javax.imageio.plugins.jpeg.JPEGImageReadParam;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestList {
+
+    public static void main(String[] args) {
+        test2();
+    }
+
+    public static void test1() {
+        List<Person> list = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+            Person p = new Person(i,"张三","99","中国","123456890");
+            list.add(p);
+        }
+        Person p2 = new Person(5,"张三","99","中国","123456890");
+
+        List<Person> remove = new ArrayList<>();
+//        remove.add(p2);
+        list.remove(p2);
+        Person rem = null;
+
+        for (Person person : list) {
+            if(person.getId() ==5){
+                remove.add(person);
+                rem = person;
+            }
+            System.out.println(person);
+        }
+//        list.removeAll(remove);
+        list.remove(rem);
+        System.out.println(list.size());
+    }
+
+    public static void test2() {
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            list.add("str-"+i);
+        }
+//        list.remove("str-5");
+
+        list.add("航三");
+        list.add("航四");
+        list.add("航一");
+        list.add("航二");
+        Collections.reverse(list);
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println(list.size());
+    }
+}
