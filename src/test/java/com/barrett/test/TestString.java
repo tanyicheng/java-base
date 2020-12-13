aa04428fd2b26b5ef4010b0593933d75dff7dd8f
barrett <yicheng.tan@seraphim-energy.com>
2020-12-13 13:58:21 +0800

    feat: 字符拼接性能测试

---
+++ src/test/java/com/barrett/test/TestString.java
@@ -0,0 +1,67 @@
+package com.barrett.test;
+
+import com.barrett.util.DateUtil;
+import com.sun.deploy.util.StringUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 字符拼接效率测试
+ * 1、执行了：22208ms！TODO  因为每次都要新建一个对象，所以效率最慢
+ * 2、执行了：22ms！
+ * 3、执行了：20ms！
+ * @author created by barrett in 2020/12/13 10:34
+ **/
+public class TestString {
+
+    private static long start = 0l;
+    private static long end = 0l;
+    private static String result = "";
+    private static int len = 100000;
+
+    @Before
+    public void before() {
+        start = System.currentTimeMillis();
+    }
+
+    @After
+    public void after() {
+        end = System.currentTimeMillis();
+        int time = (int) ((end - start));
+        System.out.println("执行了：" + time + "ms！");
+        System.out.println(result);
+    }
+
+
+    @Test
+    public void test1() {
+        String a = "a";
+        for (int i = 0; i < len; i++) {
+            a += i;
+        }
+        result = a;
+    }
+
+    @Test
+    public void test2() {
+        List<String> a = new ArrayList<>();
+        for (int i = 0; i < len; i++) {
+            a.add("a");
+        }
+        result = StringUtils.join(a, ",");
+    }
+
+    @Test
+    public void test3() {
+        String[] a = new String[len];
+        for (int i = 0; i < len; i++) {
+            a[i] = "a";
+        }
+        result = StringUtils.join(Arrays.asList(a), ",");
+    }
+}
