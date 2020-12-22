fa26af6874cebc7182c44337c94c19d942bea464
barrett <yicheng.tan@seraphim-energy.com>
2020-12-22 09:11:47 +0800

    feat: uml类图

---
+++ src/main/java/com/barrett/gof/uml类图使用/Person.java
@@ -0,0 +1,13 @@
+package com.barrett.gof.uml类图使用;
+
+public class Person{ //代码形式->类图
+	private Integer id;
+	private String name;
+	public void setName(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return  name;
+	}
+}
+
