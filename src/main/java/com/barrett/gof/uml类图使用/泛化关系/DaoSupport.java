102e8eb17e70482b5a8ece6016c8cc8845185e05
barrett <yicheng.tan@seraphim-energy.com>
2020-12-22 09:10:50 +0800

    feat: uml类图-泛化关系

---
+++ src/main/java/com/barrett/gof/uml类图使用/泛化关系/DaoSupport.java
@@ -0,0 +1,9 @@
+package com.barrett.gof.uml类图使用.泛化关系;
+
+public abstract class DaoSupport{
+	public void save(Object entity){
+	}
+	public void delete(Object id){
+	}
+}
+
---
+++ src/main/java/com/barrett/gof/uml类图使用/泛化关系/PersonServiceBean.java
@@ -0,0 +1,10 @@
+package com.barrett.gof.uml类图使用.泛化关系;
+
+/**
+ * 1) 泛化关系实际上就是继承关系
+ * 2) 如果A类继承了B类，我们就说A和B存在泛化关系
+ * @author created by barrett in 2020/12/22 09:06
+ **/
+public class PersonServiceBean extends DaoSupport {
+
+}
