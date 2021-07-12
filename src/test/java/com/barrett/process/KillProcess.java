672c49cc0b152b0a646264ae4e8f994395337dec
tanyicheng <65797706@qq.com>
2021-07-12 08:48:24 +0800

    feat: process option

---
+++ src/test/java/com/barrett/process/KillProcess.java
@@ -0,0 +1,20 @@
+package com.barrett.process;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * 进程 test
+ * @author created by barrett in 2021/7/12 08:48
+ **/
+public class KillProcess {
+
+    @Test
+    public void getProcess() throws IOException {
+        //启动应用
+        Process process = Runtime.getRuntime().exec("D:\\package\\plc-updown\\流水线上下料客户端v1.0.2.exe");
+
+        System.out.println(process);
+    }
+}
