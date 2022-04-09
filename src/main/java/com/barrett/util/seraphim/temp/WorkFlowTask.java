0998e6adfd8247e4f4eef3858f8a87390a48030b
barrett <65797706@qq.com>
2022-04-09 14:26:38 +0800

    fix: test

--- src/main/java/com/barrett/util/seraphim/temp/WorkFlowTask.java
+++ src/main/java/com/barrett/util/seraphim/temp/WorkFlowTask.java
@@ -8,37 +8,32 @@ package com.barrett.util.seraphim.temp;
 public class WorkFlowTask {
     public static final String TableName = "wf_ru_task";
 
-    //模型设计id
-    private String modelId;
-    //任务节点id
-//    private String taskId;
+    //任务实例id
+    private String taskInstId;
+
     //任务节点key
     private String taskKey;
     //任务节点顺序
     private String taskNo;
-    //类型:上级部门｜固定领导
-    private String type;
     //表达式:金额大于5000 | 请假天数大于3天
     private String expression;
     //是否会签:1是，2否
-    private String izJointly;
+    private int izJointly;
     //会签通过率：设定后满足一定比例即表示通过
-    private String passRate;
+    private Integer passRate;
     //fixme 代理人任务id
-    private String taskId;
-    //---审批人信息---添加代理人后，此审批人只能看不能签核，
+    private String agentTaskId;
+    //start---审批人信息---添加代理人后，此审批人只能看不能签核，
     //工号
     private String staffId;
     //姓名
     private String staffName;
-    //职称
+    //职称-描述信息（总监审批）
     private String position;
     //所在部门id
     private String orgId;
     //父部门id
     private String parentOrgId;
-    //部门扩展编号：上下级关系 fixme 公共库未扩展字段
-    private String extCode;
     //---end---
 
     //开始时间：任务激活后更新开始时间
@@ -47,9 +42,13 @@ public class WorkFlowTask {
     private String endTime;
     //耗时：=开始-审批
     private String timeConsuming;
-    //状态：挂起状态 1激活 2挂起，只有激活的任务才能被审批人看到
-    private String status;
+    //任务状态
+    private int status;
 
+    //签核结果,Y,N
+    private int signType;
+    //签核结果描述
+    private String remark;
 
 
 
