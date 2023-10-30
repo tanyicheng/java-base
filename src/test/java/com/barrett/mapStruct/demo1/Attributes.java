c9589dc9e53cd69b12c7f95414619280bfdaac52
yicheng.tan <65797706@qq.com>
2023-10-30 10:29:38 +0800

    feat: mapstruct 转换方式测试

---
+++ src/test/java/com/barrett/mapStruct/demo1/Attributes.java
@@ -0,0 +1,15 @@
+package com.barrett.mapStruct.demo1;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Attributes {
+    private Long id;
+    private String name;
+}
