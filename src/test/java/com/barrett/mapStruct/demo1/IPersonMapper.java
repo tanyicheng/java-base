be6cd0375c4ce0f6d819d7a0ec37f70bfacb32c4
yicheng.tan <65797706@qq.com>
2023-11-03 08:00:54 +0800

    feat: mapstruct 其他转换方式

--- src/test/java/com/barrett/mapStruct/demo1/IPersonMapper.java
+++ src/test/java/com/barrett/mapStruct/demo1/IPersonMapper.java
@@ -4,20 +4,20 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = AttributeConvertUtil.class)
 public interface IPersonMapper {
     IPersonMapper INSTANCT = Mappers.getMapper(IPersonMapper.class);
 
-    /*
-    String转日期
-    @Mapping(target = "createTime", source = "createTime", dateFormat = "yyyy-MM-dd")
-    String转数字
-    @Mapping(target = "age", source = "age", numberFormat = "#0.00")
-    忽略某个字端
-    @Mapping(target = "id", ignore = true)
-    给默认值
-    @Mapping(target = "userVerified", defaultValue = "defaultValue-2")
-     */
+    @Mapping(target = "attributes", source = "attributes", qualifiedByName = "jsonToObject")
     @Mapping(target = "userNick1", source = "userNick")
+//    String转日期
+    @Mapping(target = "createTime", source = "createTime", dateFormat = "yyyy-MM-dd")
+//    String转数字
+    @Mapping(target = "age", source = "age", numberFormat = "#0.00")
+//    忽略某个字端
+    @Mapping(target = "id", ignore = true)
+//    给默认值
+    @Mapping(target = "userVerified", defaultValue = "defaultValue-2")
     UserEntity po2entity(UserPo userPo);
+
 }
