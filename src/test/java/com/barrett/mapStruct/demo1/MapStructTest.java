b6ae8aa35d0ed59f9f06aafb07a7f3b850ed62e4
yicheng.tan <65797706@qq.com>
2023-11-08 08:05:13 +0800

    feat: mapstruct 其他转换方式

--- src/test/java/com/barrett/mapStruct/demo1/MapStructTest.java
+++ src/test/java/com/barrett/mapStruct/demo1/MapStructTest.java
@@ -13,17 +13,21 @@ public class MapStructTest {
 
     public static void testNormal() {
         System.out.println("-----------testNormal-----start------");
+        String attributes = "{\"id\":2,\"name\":\"测试123\"}";
         UserPo userPo = UserPo.builder()
                 .id(1L)
                 .gmtCreate(new Date())
+                .createTime("2023-10-01")
                 .buyerId(666L)
                 .userNick("测试mapstruct")
                 .userVerified("ok")
-                .age(18L) 
+                .age("18")
+                .attributes(attributes)
                 .build();
         System.out.println("1234" + userPo);
         UserEntity userEntity = IPersonMapper.INSTANCT.po2entity(userPo);
         System.out.println(userEntity);
         System.out.println("-----------testNormal-----ent------");
+
     }
 }
