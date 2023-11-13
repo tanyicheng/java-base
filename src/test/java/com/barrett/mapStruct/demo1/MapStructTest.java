785ce67bb99740c93cfd9a723497e8ec1efbcd53
yicheng.tan <65797706@qq.com>
2023-11-13 08:16:17 +0800

    feat: mapstruct 参数测试

--- src/test/java/com/barrett/mapStruct/demo1/MapStructTest.java
+++ src/test/java/com/barrett/mapStruct/demo1/MapStructTest.java
@@ -1,17 +1,19 @@
 package com.barrett.mapStruct.demo1;
 
+import org.junit.Test;
+import org.springframework.beans.BeanUtils;
+
 import java.util.Date;
 
 /**
  * 参考：https://juejin.cn/post/7140149801991012365
+ *
  * @author created by barrett in 2023/10/18 08:47
  */
 public class MapStructTest {
-    public static void main(String[] args) {
-        testNormal();
-    }
 
-    public static void testNormal() {
+    @Test
+    public void testNormal() {
         System.out.println("-----------testNormal-----start------");
         String attributes = "{\"id\":2,\"name\":\"测试123\"}";
         UserPo userPo = UserPo.builder()
@@ -30,4 +32,39 @@ public class MapStructTest {
         System.out.println("-----------testNormal-----ent------");
 
     }
+
+
+    @Test
+    public static void testTime() {
+        System.out.println("-----------testTime-----start------");
+        int times = 50000000;
+        final long springStartTime = System.currentTimeMillis();
+        for (int i = 0; i < times; i++) {
+            UserPo userPo = UserPo.builder()
+                    .id(1L)
+                    .gmtCreate(new Date())
+                    .buyerId(666L)
+                    .userNick("测试123")
+                    .userVerified("ok")
+                    .build();
+            UserEntity userEntity = new UserEntity();
+            BeanUtils.copyProperties(userPo, userEntity);
+        }
+        final long springEndTime = System.currentTimeMillis();
+        for (int i = 0; i < times; i++) {
+            UserPo userPo = UserPo.builder()
+                    .id(1L)
+                    .gmtCreate(new Date())
+                    .buyerId(666L)
+                    .userNick("测试123")
+                    .userVerified("ok")
+                    .build();
+            UserEntity userEntity = IPersonMapper.INSTANCT.po2entity(userPo);
+        }
+        final long mapstructEndTime = System.currentTimeMillis();
+        System.out.println("BeanUtils use time=" + (springEndTime - springStartTime) / 1000 + "秒" +
+                "; Mapstruct use time=" + (mapstructEndTime - springEndTime) / 1000 + "秒");
+        System.out.println("-----------testTime-----end------");
+    }
+
 }
