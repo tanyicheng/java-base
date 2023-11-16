47dea2cc42913ed46fe5a909b3d453c8d1183738
yicheng.tan <65797706@qq.com>
2023-11-16 15:59:20 +0800

    feat: mapstruct 性能测试

--- src/test/java/com/barrett/mapStruct/demo1/MapStructTest.java
+++ src/test/java/com/barrett/mapStruct/demo1/MapStructTest.java
@@ -39,21 +39,23 @@ public class MapStructTest {
      * @author created by barrett in 2023/11/13 08:16
      */
     @Test
-    public static void testTime() {
+    public void testTime() {
         System.out.println("-----------testTime-----start------");
         int times = 50000000;
         final long springStartTime = System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            UserPo userPo = UserPo.builder()
-                    .id(1L)
-                    .gmtCreate(new Date())
-                    .buyerId(666L)
-                    .userNick("测试123")
-                    .userVerified("ok")
-                    .build();
-            UserEntity userEntity = new UserEntity();
-            BeanUtils.copyProperties(userPo, userEntity);
-        }
+//        实测 45秒
+//        for (int i = 0; i < times; i++) {
+//            UserPo userPo = UserPo.builder()
+//                    .id(1L)
+//                    .gmtCreate(new Date())
+//                    .buyerId(666L)
+//                    .userNick("测试123")
+//                    .userVerified("ok")
+//                    .build();
+//            UserEntity userEntity = new UserEntity();
+//            BeanUtils.copyProperties(userPo, userEntity);
+//        }
+
         final long springEndTime = System.currentTimeMillis();
         for (int i = 0; i < times; i++) {
             UserPo userPo = UserPo.builder()
@@ -65,6 +67,7 @@ public class MapStructTest {
                     .build();
             UserEntity userEntity = IPersonMapper.INSTANCT.po2entity(userPo);
         }
+
         final long mapstructEndTime = System.currentTimeMillis();
         System.out.println("BeanUtils use time=" + (springEndTime - springStartTime) / 1000 + "秒" +
                 "; Mapstruct use time=" + (mapstructEndTime - springEndTime) / 1000 + "秒");
